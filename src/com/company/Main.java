83a852681e25343bd6fe3338eef13502d3d51ded
Jochem202 <56383374+Jochem202@users.noreply.github.com>
2019-10-10 10:45:58 +0200

    terug hallo allemaal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo iedereen!");
+        System.out.println("Hallo allemaal!");
     }
 }
